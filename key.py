7caf71a45ba8702c72b72d82b3138bef3dca59fe
unknown <amine.atifi@lycee-jeanrostand.fr>
2024-05-13 21:52:53 +0200

    amélioration focntion cryptage + fonction décryptage

--- key.py
+++ key.py
@@ -1,20 +1,44 @@
-def cryptage(key_phrase,caractere):
-    ascii_value = ord(caractere) +65
-    resultat = ascii_value
+def cryptage (caractere,cle):
+    resultat =(ord(caractere) + ord(cle))%127
     return resultat
-
-key_phrase = input("Veuillez entrer une clé : ")
-with open(r"C:/Users/amine/Downloads",'r') as fichier :
+key_phrase = input("Veullez entrer une clé : ")
+with open(r"C:\Users\amine\OneDrive\Documents\entrainement\u.txt",'r',encoding="utf-8") as fichier :
     contenu = fichier.read()
 
-contenu_crypte =""
-for caractere in contenu : 
-    resultat2 = cryptage(key_phrase,caractere)
-    contenu_crypte = contenu_crypte + chr(resultat2)
+contenu_crypte = ""
+for i in range(len(contenu)):
+    caractere = contenu[i]
+    cle = key_phrase[i % len(key_phrase)]
+    Resultat = cryptage(caractere,cle)
+    contenu_crypte += chr(Resultat)
 
-nouveau_fichier = input("Le fichier est désormais crypté! Veuillez le renommer :")
-with open(nouveau_fichier,'w') as fichier_crypte:
+Nouveau_fichier = input("Le fichier est crypte! Renommez-le : ")
+with open(Nouveau_fichier,'w',encoding="utf-8") as fichier_crypte :
     fichier_crypte.write(contenu_crypte)
+
+def decryptage (caractere,cle):
+    resultat =(ord(caractere) - ord(cle)+127)%127
+    return resultat
+
+with open(Nouveau_fichier,'r',encoding="utf-8") as fichier_crypte :
+    contenu = fichier_crypte.read()
+
+contenu_decrypte = ""
+for i in range(len(contenu)):
+    caractere = contenu[i]
+    cle = key_phrase[i % len(key_phrase)]
+    Resultat = decryptage(caractere,cle)
+    contenu_decrypte += chr(Resultat)
+
+NNouveau_fichier = input("Le fichier est decrypte! Renommez-le : ")
+with open(NNouveau_fichier,'w',encoding="utf-8") as fichier_decrypte :
+    fichier_decrypte.write(contenu_decrypte)
+
+
+
+
+
+
 
 
  
