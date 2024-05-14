db66add5c7faed2b7d647a30179c1aaf77cc7096
JAUNIAU MARC <jauniau_m@JEAN-ROSTAND.lan>
2024-05-14 12:26:05 +0200

    90%

--- interface.py
+++ interface.py
@@ -2,17 +2,17 @@ from tkinter import *
 from tkinter import filedialog
 from PIL import Image, ImageTk 
 
-def resize_image(image_path, width, height):
+def taille_image(image_path, width, height):
     original_image = Image.open(image_path)
-    resized_image = original_image.resize((width, height))
-    return ImageTk.PhotoImage(resized_image)
+    redimenssion_image = original_image.resize((width, height))
+    return ImageTk.PhotoImage(redimenssion_image)
 
 def parcourir():
-    filepath = filedialog.askopenfilename()
-    entry_filename.delete(0, END)
-    entry_filename.insert(0, filepath)
+    chemin_fichier = filedialog.askopenfilename()
+    entre_nomfichier.delete(0, END)
+    entre_nomfichier.insert(0, chemin_fichier)
 
-def crypter_contenu(contenu, key_phrase):
+def crypter_contenu(contenu, cle_phrase):
     contenu_crypte = ""
     for caractere in contenu:
         ascii_value = ord(caractere) + 65
@@ -21,13 +21,13 @@ def crypter_contenu(contenu, key_phrase):
     return contenu_crypte
 
 def crypter_fichier():
-    filepath = entry_filename.get()
-    key_phrase = entry_key.get()
+    chemin_fichier = entre_nomfichier.get()
+    cle_phrase = entre_cle.get()
 
-    with open(filepath, 'r', encoding="utf-8", errors='ignore') as fichier:
+    with open(chemin_fichier, 'r', encoding="utf-8", errors='ignore') as fichier:
         contenu = fichier.read()
 
-    contenu_crypte = crypter_contenu(contenu, key_phrase)
+    contenu_crypte = crypter_contenu(contenu, cle_phrase)
 
     nouveau_fichier = filedialog.asksaveasfilename(defaultextension=".txt")
     with open(nouveau_fichier, 'w', encoding="utf-8", errors='ignore') as fichier_crypte:
@@ -39,43 +39,43 @@ root.title("Logiciel de cryptage et de decryptage")
 root.resizable(width=False, height=False) #Pas de pleine écran
 
 # taile img
-crypter_photo = resize_image("cryptage-des-donnees (1).png", 100, 100)
-decrypter_photo = resize_image("decryptage.png", 100, 100)
+crypter_photo = taille_image("cryptage-des-donnees (1).png", 100, 100)
+decrypter_photo = taille_image("decryptage.png", 100, 100)
 
 # Cadre pour bouton
-frame_buttons = Frame(root)
-frame_buttons.pack(pady=10)
+cadre_buttons = Frame(root)
+cadre_buttons.pack(pady=10)
 
 #Btn Crypter
-btn_crypter = Button(frame_buttons, image=crypter_photo, command=crypter_fichier)
+btn_crypter = Button(cadre_buttons, image=crypter_photo, command=crypter_fichier)
 btn_crypter.pack(side=LEFT, padx=10)
 
 # Bouton Décrypter
-btn_decrypter = Button(frame_buttons, image=decrypter_photo, command=decrypter_photo)
+btn_decrypter = Button(cadre_buttons, image=decrypter_photo, command=decrypter_photo)
 btn_decrypter.pack(side=RIGHT, padx=10)
 
 # Cadre pour le parcourir
-frame_file = Frame(root)
-frame_file.pack(pady=10)
+cadre_fichier = Frame(root)
+cadre_fichier.pack(pady=10)
 
-label_filename = Label(frame_file, text="Sélectionnez le fichier :")
-label_filename.grid(row=0, column=0, padx=5, sticky="w")
+etiquette_nomfichier = Label(cadre_fichier, text="Sélectionnez le fichier :")
+etiquette_nomfichier.grid(row=0, column=0, padx=5, sticky="w")
 
-entry_filename = Entry(frame_file, width=40)
-entry_filename.grid(row=0, column=1, padx=5)
+entre_nomfichier = Entry(cadre_fichier, width=40)
+entre_nomfichier.grid(row=0, column=1, padx=5)
 
-btn_parcourir = Button(frame_file, text="Parcourir", command=parcourir)
+btn_parcourir = Button(cadre_fichier, text="Parcourir", command=parcourir)
 btn_parcourir.grid(row=0, column=2, padx=5)
 
 # Cadre pour la clé de cryptage
-frame_key = Frame(root)
-frame_key.pack(pady=10)
+cadre_cle = Frame(root)
+cadre_cle.pack(pady=10)
 
-label_key = Label(frame_key, text="Entrez la clé :")
+label_key = Label(cadre_cle, text="Entrez la clé :")
 label_key.grid(row=0, column=0, padx=5, sticky="w")
 
-entry_key = Entry(frame_key, width=40)
-entry_key.grid(row=0, column=1, padx=5)
+entre_cle = Entry(cadre_cle, width=40)
+entre_cle.grid(row=0, column=1, padx=5)
 
 # Bouton Quitter
 btn_quitter = Button(root, text="Quitter", command=root.destroy)
