6f8d5664d281b6724dea46deb18ba427223fc3b0
unknown <amine.atifi@lycee-jeanrostand.fr>
2024-05-19 22:45:13 +0200

    TEST fonction cryptagee

--- cryptage.py
+++ cryptage.py
@@ -1,17 +1,32 @@
-def cryptage_caractere (caractere):
-    resultat =(ord(caractere) + 65 )%127
-    if caractere.isupper():
-        resultat += 32
+'''
+-cryptage_caractere() > fonction pour crypter les caracteres
+-(caracteres) > 1er argument de la fonction correspodnant aux données entrés par utilisateur
+-(cle_phrase) > 2eme argument correspodnant a la clé entrée par l'utilisateur
+-for i in range(len(caracteres)): > on applique la fonction sur tous les caracteres inscris par l'utilisateur
+- caractere = caracteres[i] > un caractere correspind au premier caractere de la phrase entrée par l'utilisateur
+- ascii_value = (ord(caractere) + ord(cle_caractere) + 3) % 256 > 
+'''
+def cryptage_caractere(caracteres, cle_phrase):
+    contenu_crypte = ""
+    for i in range(len(caracteres)):
+        caractere = caracteres[i]
+        cle_caractere = cle_phrase[i % len(cle_phrase)]
+        ascii_value = (ord(caractere) + ord(cle_caractere) + 3) 
+        contenu_crypte += chr(ascii_value)
+    return contenu_crypte
 
-    return resultat
+caracteres = input("Veuillez entrer des caractères : ")
+cle_phrase = input("Veuillez entrer une clé : ")
 
-caracteres = input("Veuillez entrer des caracteres : ")
-contenu_crypte = ""
-for i in range(len(caracteres)):
-    caractere_crypte = caracteres[i]
-    Resultat = cryptage_caractere(caractere_crypte)
-    contenu_crypte += chr(Resultat)
-print (contenu_crypte)
+
+resultat_crypte = cryptage_caractere(caracteres, cle_phrase)
+print("Le texte crypté est :", resultat_crypte)
+
+
+
+
+    
+    
 
 
 
