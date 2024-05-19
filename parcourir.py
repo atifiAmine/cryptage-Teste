53563abea35f1fd28395aa6e1b2a294ad42c8f09
unknown <amine.atifi@lycee-jeanrostand.fr>
2024-05-19 13:46:09 +0200

    TEST fonction parcourir

--- parcourir.py
+++ parcourir.py
@@ -1,8 +1,19 @@
-def parcourir(fichier_selectionne):
-    with open(fichier_selectionne,'r',encoding="utf-8") as fichier :
+'''
+-parcourir() > fonction pour sélectionner le fichier 
+- fichier_selectionne > argument corrspondant au fichier sélectionné 
+-with open > on ouvre ce fichier
+-contenu = fichier.read > on met ce fichier en mode lecture
+'''
+from tkinter import *
+from tkinter import filedialog
+def parcourir():
+    chemin_fichier = filedialog.askopenfilename()
+    with open(chemin_fichier, 'r', encoding="utf-8", errors="ignore") as fichier:
         contenu = fichier.read()
-        return contenu 
-  
-nom_fichier = r"C:\Users\atifi_a\Documents\capteurSR04\projet\fichier.txt"
-contenu_fichier = parcourir(nom_fichier)
-print(contenu_fichier)
+        print(contenu)
+'''
+-on rapelle la fonction "parcourir" et on stcoke dans résultat
+-print resultat > on retourne "resultat", soit le contenu du fichier sélectionné 
+'''
+resultat= parcourir()
+print (resultat)
